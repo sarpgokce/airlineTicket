b6f646192a96f390b85274899c82de204d03d3b5
Sg <Sg@DESKTOP-3H8UP4J>
2022-11-21 04:57:30 +0300

    beginning

---
+++ Airline/src/Pages/Main.java
@@ -0,0 +1,79 @@
+package Pages;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JDesktopPane;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 500);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Customer");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Add Customer");
+		mnNewMenu.add(mntmNewMenuItem_4);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Search Customer");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu_1 = new JMenu("Tickets");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Book Ticket");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Flight");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Add Flight");
+		mnNewMenu_2.add(mntmNewMenuItem_2);
+		
+		JMenu mnNewMenu_3 = new JMenu("User");
+		menuBar.add(mnNewMenu_3);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("User Creation");
+		mnNewMenu_3.add(mntmNewMenuItem_3);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBounds(0, 0, 984, 439);
+		contentPane.add(desktopPane);
+	}
+}
---
+++ Airline/src/Pages/addCustomer.java
@@ -0,0 +1,132 @@
+package Pages;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import java.awt.Panel;
+import java.awt.Color;
+import java.awt.Label;
+import javax.swing.JTextField;
+import java.awt.TextArea;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+
+public class addCustomer extends JInternalFrame {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					addCustomer frame = new addCustomer();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public addCustomer() {
+		setBounds(100, 100, 655, 429);
+		getContentPane().setLayout(null);
+		
+		Panel panel = new Panel();
+		panel.setBackground(new Color(0, 0, 255));
+		panel.setBounds(36, 61, 265, 240);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		Label label = new Label("First Name");
+		label.setForeground(new Color(255, 255, 255));
+		label.setBounds(20, 22, 62, 22);
+		panel.add(label);
+		
+		Label label_1 = new Label("Last Name");
+		label_1.setForeground(new Color(255, 255, 255));
+		label_1.setBounds(20, 43, 62, 22);
+		panel.add(label_1);
+		
+		Label label_2 = new Label("NIC No");
+		label_2.setForeground(new Color(255, 255, 255));
+		label_2.setBounds(20, 65, 62, 22);
+		panel.add(label_2);
+		
+		Label label_3 = new Label("Passport ID");
+		label_3.setForeground(new Color(255, 255, 255));
+		label_3.setBounds(20, 86, 62, 22);
+		panel.add(label_3);
+		
+		Label label_4 = new Label("Adress");
+		label_4.setForeground(new Color(255, 255, 255));
+		label_4.setBounds(20, 108, 62, 22);
+		panel.add(label_4);
+		
+		textField = new JTextField();
+		textField.setBounds(88, 22, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(88, 45, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(88, 67, 86, 20);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(88, 88, 86, 20);
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		TextArea textArea = new TextArea();
+		textArea.setBounds(87, 111, 136, 61);
+		panel.add(textArea);
+		
+		Label label_5 = new Label("Customer ID");
+		label_5.setFont(new Font("Dialog", Font.BOLD, 16));
+		label_5.setBounds(36, 22, 116, 22);
+		getContentPane().add(label_5);
+		
+		Label label_6 = new Label("New label");
+		label_6.setForeground(new Color(255, 0, 0));
+		label_6.setFont(new Font("Dialog", Font.PLAIN, 16));
+		label_6.setBounds(202, 22, 62, 22);
+		getContentPane().add(label_6);
+		
+		Panel panel_1 = new Panel();
+		panel_1.setBackground(new Color(0, 0, 255));
+		panel_1.setBounds(307, 61, 305, 240);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Date Of Birth");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(26, 42, 63, 14);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Gender");
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(26, 90, 46, 14);
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Contact");
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setBounds(26, 139, 46, 14);
+		panel_1.add(lblNewLabel_2);
+
+	}
+}
